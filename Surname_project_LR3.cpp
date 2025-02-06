9b81e365b38874400ed778c8a732d857eab101be
YaschunTV <yaschuntv@mgupp.ru>
2025-02-06 11:36:31 +0300

    Add void calcArea

--- Surname_project_LR3.cpp
+++ Surname_project_LR3.cpp
@@ -11,14 +11,11 @@ int side1, side2;
 //функция ввода данных
 function<void()> EnterNumber(int& varLink, string label) {
 	//разрабатывается Сидоровым С. - ветка branch_fun_1    
-    return [&varLink, label]() {
-        std::cout << label << " = ";
-        std::cin >> varLink;
-    };
 }
 void CalcArea()
 {
 	//разрабатывается Петровым П. - ветка branch_fun_2
+    cout << "area = " << side1 * side2 << endl;
 }
 
 void CalcPerim()
